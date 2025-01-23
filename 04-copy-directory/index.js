294c13d483af0af2493cd500bfc0fac6e903a9cd
Absolute-2004 <mixalobach@gmail.com>
2025-01-23 21:10:03 +0300

    fix: if you delete the file in the files folder and run the function again, create copy update

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,11 +1,19 @@
-function copyDir(src) {
-  const fs = require('fs');
-  const path = require('path');
-  fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+const fs = require('fs');
+const path = require('path');
+fs.rm(
+  path.join(__dirname, 'files-copy'),
+  { recursive: true, force: true },
+  (err) => {
     if (err) throw err;
-  });
+    copyDir('files');
+  },
+);
+function copyDir(src) {
   const srcDir = path.join(__dirname, src);
   const distDir = path.join(__dirname, 'files-copy');
+  fs.mkdir(distDir, { recursive: true }, (err) => {
+    if (err) throw err;
+  });
   fs.readdir(srcDir, (err, files) => {
     if (err) throw err;
     files.forEach((file) => {
@@ -17,4 +25,3 @@ function copyDir(src) {
     });
   });
 }
-copyDir('files');
